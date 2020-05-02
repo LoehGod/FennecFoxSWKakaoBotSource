a0ad1db5f626a17cb226e3ef0070eafc21da1ccc
FennecFoxSW <FennecFoxSW@protonmail.com>
2020-05-02 09:43:09 +0900

    Update NaverRealtimeQuery.js
    Import org.jsoup package

--- NaverRealtimeQuery.js
+++ NaverRealtimeQuery.js
@@ -1,9 +1,11 @@
+importPackage(org.jsoup)
+
 function response(room, msg, sender, isGroupChat, replier) {
   if (msg.trim() == "!실시간검색어") {
     var url = "https://datalab.naver.com/keyword/realtimeList.naver?entertainment=2&groupingLevel=0&marketing=2&news=2&sports=2";
     var document = null;
     try {
-      document = org.jsoup.Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36").get();
+      document = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36").get();
     } catch (error) {
       replier.reply("뭔가 문제가 발생했습니다.\n" + error);
     }
